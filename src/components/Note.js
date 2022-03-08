c9dc4311041249a6a0374ce77905ce422a9de6c9
Bartosz Zakrzewski <barzak200@gmail.com>
2022-03-08 18:39:21 +0100

    Style note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+const maxNoteLength = 200;
+
 const Note = (props) => {
+  const note = props.note;
+  const content =
+    note.content.length > maxNoteLength
+      ? note.content.substring(0, maxNoteLength) + '...'
+      : note.content;
+
   return (
     <li className="note">
-      <div className="note-content">{props.note.content}</div>
-      <div className="note-date">{props.note.date}</div>
-      <button className="btn btn-delete">Delete note</button>
+      <div className="note-content">
+        <p>{content}</p>
+        <p className="note-date">{note.date}</p>
+      </div>
+      <div className="note-button-container">
+        <button className="btn btn-delete">Delete note</button>
+      </div>
     </li>
   );
 };
