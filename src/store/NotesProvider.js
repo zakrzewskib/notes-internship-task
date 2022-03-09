a36e4ddf87f503bdb533a799e3eae3139c9c5b66
Bartosz Zakrzewski <barzak200@gmail.com>
2022-03-09 09:52:14 +0100

    Change order of notes - new added note is on top

--- src/store/NotesProvider.js
+++ src/store/NotesProvider.js
@@ -4,18 +4,18 @@ import NotesContext from './notes-context';
 const NOTES_MOCK = [
   {
     id: '1',
-    date: '06/19/2020',
+    date: '06/17/2020',
     content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
   },
   {
     id: '2',
-    date: '06/18/2020',
+    date: '06/19/2020',
     content:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nobis, quia earum sunt placeat modi illum fugit **aliquid labore nesciunt** in ullam totam tenetur hic voluptatem autem ratione. Modi, exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis nobis, quia earum sunt placeat modi illum fugit aliquid labore nesciunt in ullam totam tenetur hic voluptatem autem ratione. Modi, exercitationem.',
   },
   {
     id: '3',
-    date: '06/17/2020',
+    date: '06/20/2020',
     content: '# Hello World',
   },
 ];
@@ -35,7 +35,7 @@ const notesReducer = (state, action) => {
   }
 
   if (action.type === 'ADD') {
-    const updatedNotes = [...state.notes, action.note];
+    const updatedNotes = [action.note, ...state.notes];
     return {
       notes: updatedNotes,
     };
